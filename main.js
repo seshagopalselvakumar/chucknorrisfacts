f274924b6bf3968ea550231873d21e9196943fda
Seshagopal Selvakumar <seshagopalselvakumar@gmail.com>
2020-09-20 18:44:08 +0530

    Added files

---
+++ main.js
@@ -0,0 +1,12 @@
+showFact=()=>{
+    let displayFact = document.querySelector(".display-facts");
+    let url = "https://api.chucknorris.io/jokes/random";
+    fetch(url)
+        .then(response=>{
+            return response.json()
+        })
+        .then(responseData=>{
+            console.log("response", responseData);
+            displayFact.innerHTML = responseData.value;
+        })
+}
